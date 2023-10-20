a3347b306f4ce8a7a3e351013e6c0efc9a6fabf5
akhoz <adrian25092005@gmail.com>
2023-10-20 16:36:40 -0700

    games are now correctly identified

--- GameCrawler/GameCrawler/spiders/g2a.py
+++ GameCrawler/GameCrawler/spiders/g2a.py
@@ -1,13 +1,21 @@
 import scrapy
 
-
 class G2aSpider(scrapy.Spider):
     name = "g2a"
     start_urls = ["https://www.g2a.com/es/category/games-c189?drm%5B0%5D=273&f%5Bdrm%5D%5B0%5D=8586&region%5B0%5D=878&region%5B1%5D=8355"]
 
     def parse(self, response):
-        for game in response.xpath('//*[@id="c5a40d45-7a70-4e3b-9fa2-ac3dfbdefda3"]/section/div/div[2]/section/div[2]/div/ul'):
-            print(game)
-
-
-
+        games = response.xpath('//li[contains(@class, "indexes__StyledProductBox-wklrsw-91")]')
+        
+        for game in games:
+            game_name = game.xpath('@name').extract_first()
+            game_price = game.xpath('.//span[contains(@data-locator, "zth-price")]/text()').extract_first()
+            game_discount = game.xpath('.//span[contains(@data-locator, "zth-badge")]/text()').extract_first()
+            
+            if game_name and game_price:
+                print(f"Nombre: {game_name}")
+                print(f"Precio: {game_price}")
+                
+                if game_discount:
+                    print(f"Discount: {game_discount}")
+                
