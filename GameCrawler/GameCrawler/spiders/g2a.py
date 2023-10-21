312c576ef17e701173718bf215f00ea3aa54555b
akhoz <adrian25092005@gmail.com>
2023-10-20 17:30:46 -0700

    implementing json storage

--- GameCrawler/GameCrawler/spiders/g2a.py
+++ GameCrawler/GameCrawler/spiders/g2a.py
@@ -1,8 +1,12 @@
 import scrapy
+import json
 
 class G2aSpider(scrapy.Spider):
     name = "g2a"
     start_urls = ["https://www.g2a.com/es/category/games-c189?drm%5B0%5D=273&f%5Bdrm%5D%5B0%5D=8586&region%5B0%5D=878&region%5B1%5D=8355"]
+    
+    def __init__(self):
+        self.data = []
 
     def parse(self, response):
         games = response.xpath('//li[contains(@class, "indexes__StyledProductBox-wklrsw-91")]')
@@ -14,8 +18,22 @@ class G2aSpider(scrapy.Spider):
             game_discount = game_discount.replace('-', '')  # Eliminar el signo "-"
             
             if game_name and game_price:
-                print(f"Name: {game_name}")
-                print(f"Price: {game_price}")
+                item = {
+                    "Name": game_name,
+                    "Price": game_price,
+                }
                 
                 if game_discount:
-                    print(f"Discount: {game_discount}")
+                    item["Discount"] = game_discount
+                
+                self.data.append(item)
+                print(item)
+                
+        next_page_url = response.xpath('//a[contains(@class, "nextButton")]/@href').extract_first()
+        if next_page_url:
+            yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
+
+    def closed(self, reason):
+        # Guardar los datos recopilados en un archivo JSON
+        with open('GameCrawler/outputs/g2a_data.json', 'w', encoding='utf-8') as json_file:
+            json.dump(self.data, json_file, ensure_ascii=False, indent=4)
