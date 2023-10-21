7b2739a87c458db52557e8323c9cacf0026c4cef
akhoz <adrian25092005@gmail.com>
2023-10-21 16:07:55 -0700

    Adding Non Standar User

--- GameCrawler/GameCrawler/settings.py
+++ GameCrawler/GameCrawler/settings.py
@@ -91,3 +91,4 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+USER_AGENT = 'non_standar_user (https://howlongtobeat.com/?q=recently%2520updated)'
