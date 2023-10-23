a6f4509dcba51a2e0ef812d55553f6041627accb
Hector Alonso Caravaca Vargas <hcaravacavargas@gmail.com>
2023-10-22 20:50:18 -0600

    Change to Bootstrap

--- Website/script.js
+++ Website/script.js
@@ -1,9 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
     var contentDiv = document.getElementById("content");
 
-    // Array to store games in the cart
-    var cart = [];
-
     // FUNCTION TO LOAD DATA FROM THE JSON FILE
     function loadData() {
         fetch("../GameCrawler/GameCrawler/outputs/g2a_data.json")
@@ -13,28 +10,21 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(function (data) {
                 for (var i = 0; i < data.length; i++) {
                     var game = data[i];
-                    var div = document.createElement("div");
-                    div.className = "games";
-                    div.innerHTML = `
-                        <h2>${game.Name}</h2>
-                        <p>Price: $${game.Price}</p>
-                        <p>Discount: ${game.Discount}%</p>
-                        <button class="add-to-cart" data-id="${game.ID}">Add to Cart</button>
+                    var card = document.createElement("div");
+                    card.className = "card-game card"; // Agrega las clases de estilo
+                    card.style.width = "18rem";
+
+                    card.innerHTML = `
+                        <img src="https://via.placeholder.com/150" class="card-game-img card-img-top" alt="${game.Name}">
+                        <div class="card-body">
+                            <h5 class="card-game-title card-title">${game.Name}</h5>
+                            <p class="card-game-price card-text">Price: $${game.Price}</p>
+                            <p class="card-game-discount card-text">Discount: ${game.Discount}</p>
+                            <a href="#" class="btn btn-primary add-to-cart" data-id="${game.ID}">Add to Cart</a>
+                        </div>
                     `;
-                    contentDiv.appendChild(div);
 
-                    var addToCartButton = div.querySelector('.add-to-cart');
-                    addToCartButton.addEventListener('click', function (event) {
-                        var gameId = event.target.getAttribute('data-id');
-                        var game = data.find(function (game) {
-                            return game.ID == gameId;
-                        });
-
-                        // Add the game to the cart
-                        cart.push(game);
-                        alert("Game added to the cart:", game);
-
-                    });
+                    contentDiv.appendChild(card);
                 }
             })
             .catch(function (error) {
