ef32f768702d6f6a87a5d04402bd3dd8f86a1dfc
akhoz <adrian25092005@gmail.com>
2023-10-23 17:26:56 -0700

    Adding new games to the list

--- GameCrawler/GameCrawler/games.py
+++ GameCrawler/GameCrawler/games.py
@@ -99,5 +99,14 @@ allowed_games = [
     "Football Manager 2024",
     "Terraria",
     "Payday 2",
-    "Rust"
+    "Rust",
+    "The Legend of Zelda: Ocarina of Time",
+    "The Legend of Zelda: A Link to the Past",
+    "The Legend of Zelda: Majora's Mask",
+    "The Legend of Zelda: Twilight Princess",
+    "The Legend of Zelda: The Wind Waker",
+    "The Legend of Zelda: A Link Between Worlds",
+    "The Legend of Zelda: Phantom Hourglass",
+    "The Legend of Zelda: Spirit Tracks",
+    "The Legend of Zelda: Skyward Sword"
 ]
