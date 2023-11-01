860eb9abc6693aa48ebac04d76525049ea812e2c
akhoz <adrian25092005@gmail.com>
2023-11-01 04:15:03 -0700

    Fixing troubles releated to hltb spider

--- GameCrawler/GameCrawler/spiders/howlongtobeat.py
+++ GameCrawler/GameCrawler/spiders/howlongtobeat.py
@@ -1,7 +1,7 @@
 import scrapy
 import time
 import json
-from GameCrawler.games import allowed_games
+from games import allowed_games
 import os
 
 from selenium import webdriver
@@ -15,7 +15,7 @@ class HowlongtobeatSpider(scrapy.Spider):
     start_urls = ["https://howlongtobeat.com/?q="]
 
     custom_settings = {
-        'USER_AGENT':  'non_standar_user (https://howlongtobeat.com/?q=recently%2520updated)'
+        'USER_AGENT':  'non_standar_user (https://howlongtobeat.com/?q=)'
     }
 
     def __init__(self):
@@ -24,24 +24,6 @@ class HowlongtobeatSpider(scrapy.Spider):
 
     def parse(self, response):
         game_items = response.xpath('//li[contains(@class, "back_darkish GameCard_search_list__IuMbi")]')
-        print(type(game_items))
-        print(len(game_items))
-
-        for game in game_items:
-            game_name = game.xpath('.//h3/a/@title').get()
-            completionist_time = game.xpath('.//div[contains(text(), "Completionist")]/following-sibling::div[contains(@class, "time")]/text()').get()
-
-            if game_name and completionist_time:
-                if game_name not in self.scraped_game_names:
-                    item = {
-                        'Game Name': game_name.strip(),
-                        'Completionist Hours': completionist_time.strip()
-                    }
-
-                    if game_name in allowed_games:
-                        self.data.append(item)
-                        self.scraped_game_names.add(game_name)
-                        print(f"HowLongToBeat: {game_name} -----------------------")
 
         driver = webdriver.Edge()
         driver.get(response.url)
@@ -51,7 +33,6 @@ class HowlongtobeatSpider(scrapy.Spider):
             search_box = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME,"MainNavigation_search_box__UUnYc")))
 
             response = scrapy.Selector(text=driver.page_source)
-            game_items = response.xpath('//li[contains(@class, "back_darkish GameCard_search_list__IuMbi")]')
 
             search_box.clear()
             search_box.send_keys(game_name)
@@ -63,6 +44,7 @@ class HowlongtobeatSpider(scrapy.Spider):
                 completionist_time = game.xpath('.//div[contains(text(), "Completionist")]/following-sibling::div[contains(@class, "time")]/text()').get()
 
                 if game_name and completionist_time:
+                    completionist_time = completionist_time.replace("½", ".5")
                     if game_name not in self.scraped_game_names:
                         item = {
                             'Game Name': game_name.strip(),
